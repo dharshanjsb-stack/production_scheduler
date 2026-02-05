f3bf26349e75f129b2a0ff822021fd71f2857420
dharshanjsb-stack <dharshanjsb@gmail.com>
2026-02-05 15:47:31 +0530

    Update create_production_scheduler_board.py

--- production_scheduler/patches/create_production_scheduler_board.py
+++ production_scheduler/patches/create_production_scheduler_board.py
@@ -1,1 +1,13 @@
+import frappe
 
+def execute():
+    if not frappe.db.exists("Page", "production-scheduler-board"):
+        page = frappe.get_doc({
+            "doctype": "Page",
+            "name": "production-scheduler-board",
+            "page_name": "production-scheduler-board",
+            "title": "Production Scheduler Board",
+            "module": "Production Scheduler",
+            "standard": 1
+        })
+        page.insert(ignore_permissions=True)
