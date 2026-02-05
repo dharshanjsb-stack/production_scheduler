dbec63f0bbf33ea5d2813bc6f8564b8b8a08fd28
dharshanjsb-stack <dharshanjsb@gmail.com>
2026-02-05 10:09:21 +0530

    Update hooks.py

--- production_scheduler/hooks.py
+++ production_scheduler/hooks.py
@@ -1,10 +1,6 @@
-# hooks.py
-
 app_name = "production_scheduler"
-
-# This link's the CSS to your specific custom page
-page_js = {"production-queue" : "public/js/production_queue.js"}
-page_css = {"production-queue" : "public/css/production_queue.css"}
-
-# If you want these styles to be available everywhere in the Desk:
-# app_include_css = "/assets/production_scheduler/css/production_queue.css"
+app_title = "Production Scheduler"
+app_publisher = "Dharshan JSB Stack"
+app_description = "Custom Production Scheduling App"
+app_email = "dharshanjsb@gmail.com"
+app_license = "MIT"
